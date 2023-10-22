22fdf87d40965fc8b844ec008b4429ddef5875cd
walterpaulo <walter0paulo@hotmail.com>
2023-10-22 00:58:54 -0300

    feat: new data

--- src/main/java/br/com/walterpaulo/userservice/domain/entity/Endereco.java
+++ src/main/java/br/com/walterpaulo/userservice/domain/entity/Endereco.java
@@ -1,16 +1,21 @@
 package br.com.walterpaulo.userservice.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-// @Entity
-// @Table(name = "endereco")
+@Entity
+@Table(name = "endereco")
 public class Endereco {
 
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String rua;
     private String numero;
--- src/main/java/br/com/walterpaulo/userservice/domain/entity/User.java
+++ src/main/java/br/com/walterpaulo/userservice/domain/entity/User.java
@@ -3,24 +3,31 @@ package br.com.walterpaulo.userservice.domain.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-// @Entity
-// @Table(name = "user")
+@Entity
+@Table(name = "usuario")
 public class User {
 
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String sobrenome;
     private String email;
     private String cpf;
     private LocalDateTime dataCadastro;
-    // @OneToMany
-    // @JoinColumn(name = "paciente_id")
+    @OneToMany
+    @JoinColumn(name = "enderco_id")
     private List<Endereco> enderecos;
 }
